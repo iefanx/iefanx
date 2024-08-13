424dc70a8eadcd5f5dfa2a41b959590d0d9384ac
iefan <66840141+iefanx@users.noreply.github.com>
2024-08-13 22:11:51 +0000

    main

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Fetch the Markdown file
-fetch('content.md')
-    .then(response => response.text())
-    .then(text => {
-        // Convert Markdown to HTML using marked.js
-        const htmlContent = marked(text);
-        // Insert the HTML content into the page
-        document.getElementById('content').innerHTML = htmlContent;
-    })
-    .catch(error => console.error('Error fetching markdown file:', error));
+import { content } from "./profileMd.js"
+
+document.getElementById('content').innerHTML =
+  marked.parse(content);
